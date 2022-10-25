8eb3ad41408be4d9beb8e46e28c5058240196278
lloydy8 <109030115+lloydy8@users.noreply.github.com>
2022-10-26 00:21:43 +0100

    Update chart4.js

--- charts/chart4.js
+++ charts/chart4.js
@@ -65,8 +65,8 @@ svg4.selectAll("bar")
 .on('mousemove' , function(d , i){
     d3.select("#tooltip").style("opacity", 1)
     .html("Episode: " + i.episode +  "<br/>" + "Score: " + i.score2+  "<br/>" + "Reward: " + i.rewards2 + "<br/>" + "Length: " + i.length2)
-    .style("left", (d.pageX + 10) + "px")
-    .style("top", (d.pageY - 15) + "px");
+    .style("left", (d.pageX + 180) + "px")
+    .style("top", (d.pageY + 950) + "px");
 })
 .on('mouseout' , function(){
     d3.select("#tooltip").style("opacity", 0)
